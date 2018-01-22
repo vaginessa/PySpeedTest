28483799832cd5a16f53b9299467dce1046f412d
Misha Turnbull <mishaturnbull@gmail.com>
2018-01-22 13:37:48 -0600

    Remove `settings.py`; add use of `argparse`
    Switch from defined settings to using command line argument parser.

--- main.py
+++ main.py
@@ -10,8 +10,40 @@ accompanying `settings.py` file.
 import datetime
 import time
 import sys
+import argparse
 
-from settings import REC_FILE, LOCATION, FREQ, VERBOSITY, FORCE_SERVER
+#from settings import REC_FILE, LOCATION, FREQ, VERBOSITY, FORCE_SERVER
+
+parser = argparse.ArgumentParser(description=
+                                 "Run continous internet speed tests.")
+parser.add_argument('-o',
+                    default='speed_record.ilog',
+                    help="name of the file to write to",
+                    metavar="output filename",
+                    dest='REC_FILE')
+parser.add_argument('-l', '--location',
+                    default='No location',
+                    help="where the testing is taking place",
+                    metavar="location",
+                    dest='LOCATION')
+parser.add_argument('-f', '--freq',
+                    default=0.5,
+                    type=float,
+                    help="frequency of testing, in minutes",
+                    metavar="frequency",
+                    dest='FREQ')
+parser.add_argument('-v',
+                    default=0,
+                    type=int,
+                    help="verbosity level.  [1, 3]",
+                    metavar="verbosity",
+                    dest='VERBOSITY')
+parser.add_argument('-s', '--server',
+                    default='',
+                    help="optionally specify a testing server",
+                    metavar='server',
+                    dest='FORCE_SERVER')
+args = parser.parse_args()
 
 try:
     import pyspeedtest
@@ -24,15 +56,18 @@ except ModuleNotFoundError:
 # lazy man's debugging
 def dprint(level, msg):
     """Debug printer.  Args are [level], [message]."""
-    if VERBOSITY >= level:
+    if args.VERBOSITY >= level:
         print(msg)
 
 def make_speedtest_object():
     speed_tester = pyspeedtest.SpeedTest()
 
+    if args.FORCE_SERVER == '':
+        return speed_tester
+
     # Yes, I know this is ultra-bad practice.  I'm overriding the setting
     # without editing the source of pyspeedtest.
-    speed_tester._host = FORCE_SERVER # pylint: disable=W0212
+    speed_tester._host = args.FORCE_SERVER # pylint: disable=W0212
 
     return speed_tester
 
@@ -51,18 +86,18 @@ def test_once():
         down = round(speed_tester.download(), 1)
         dprint(3, "About to upload...")
         upload_speed = round(speed_tester.upload(), 1)
-        newline = ", ".join([curtime, LOCATION, str(ping), str(down), str(upload_speed)]) \
+        newline = ", ".join([curtime, args.LOCATION, str(ping), str(down), str(upload_speed)]) \
                   + '\n'
     except Exception as exc:
         dprint(1, "It didn't work!  Joining error line...")
-        newline = ", ".join([curtime, LOCATION, exc.__repr__()]) + '\n'
+        newline = ", ".join([curtime, args.LOCATION, exc.__repr__()]) + '\n'
 
     time_b = datetime.datetime.now()
 
     dprint(2, "Test completed, result:")
     dprint(1, newline)
 
-    time_diff = (FREQ * 60) - (time_b - time_a).total_seconds()
+    time_diff = (args.FREQ * 60) - (time_b - time_a).total_seconds()
     if time_diff < 0:
         time_diff = 0  # hope we catch up eventually
 
@@ -73,7 +108,7 @@ def main():
 
     while True:
         newline, time_diff = test_once()
-        with open(REC_FILE, 'a') as record:
+        with open(args.REC_FILE, 'a') as record:
             record.write(newline)
         time.sleep(time_diff)
 
